787ed2bc5b21cce3ec1f9844d424d3e9a25e36ae
Rika Ciminieri <rikaciminieri@gmail.com>
2021-05-25 22:01:42 -0700

    Data Fetched

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import APODContainer from './components/APODContainer'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch((err)=> console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <APODContainer data={data} />
     </div>
   );
 }
---
+++ src/components/APODContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const APODContainer = (props) => {
+  const { data } = props;
+  return (
+    <div>
+      <p>i hate this</p>
+    </div>
+  );
+};
+
+export default APODContainer;
